68c11d9e7a6e6f8052edf80c00bd9f1ed0cfcbc5
fengnovo <fengnoku@126.com>
2017-05-25 23:51:53 +0800

    pull refresh

---
+++ 20170525/zhihu/index.android.js
@@ -0,0 +1,16 @@
+import React, {  
+    Component,  
+} from 'react';  
+  
+import {  
+     AppRegistry,  
+     Image,  
+     StyleSheet,  
+     Text,  
+     View,  
+     ListView,  
+ } from 'react-native';  
+  
+import App from './src/App'
+
+AppRegistry.registerComponent('zhihu', () => App);
